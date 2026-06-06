286919643db0ea1283100386c667ef7aa90e2cef
nouf <ialharbinouf.22@gmail.com>
2026-06-06 23:04:11 +0300

    Add files via upload

--- test.py
+++ test.py
@@ -1,10 +1,15 @@
-# test.py 
+# test.py
 
-def welcome():
-    return "تست تست تست"
+def welcome_message():
+    return "تست تست تست "
 
-def add(a, b):
+def add_numbers(a, b):
     return a + b
 
-print(welcome())
-print(f"5 + 3 = {add(5, 3)}")
+def say_hello(name):
+    return f"Hello {name}!"
+
+if __name__ == "__main__":
+    print(welcome_message())
+    print(f"5 + 3 = {add_numbers(5, 3)}")
+    print(say_hello("GitHub"))
